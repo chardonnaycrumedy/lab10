a653d66c6958ff6923a4e27cbd7942d6b83bd3cc
chardonnaycrumedy <s581084@student.roundrockisd.org>
2014-11-06 07:00:18 -0600

    70-100pt (done)

--- 70-100pt.py
+++ 70-100pt.py
@@ -25,12 +25,19 @@ drawpad.grid(row=0, column=1)
 # Insert your code here to draw the house!
 
 #house and roof
-rectangle = drawpad.create_rectangle(200,200,400,400)
-line = drawpad.create_line(200,200,300,100)
-line = drawpad.create_line(300,100,400,200)
+rectangle = drawpad.create_rectangle(200,200,400,400,fill="red")
+line = drawpad.create_line(200,200,300,100,fill="red")
+line = drawpad.create_line(300,100,400,200,fill="red")
+line = drawpad.create_line(200,100,200,200,fill="red")
+line = drawpad.create_line(200,100,220,100,fill="red")
+line = drawpad.create_line(220,100,220,180,fill="red")
+rectangle = drawpad.create_rectangle(180,400,420,420,fill="green")
 #door
 rectangle = drawpad.create_rectangle(270,390,320,320)
+oval = drawpad.create_oval(310,350,320,360,fill="yellow")
 #window
-rectangle = drawpad.create_rectangle(270,390,270,320)
+rectangle = drawpad.create_rectangle(220,220,280,260)
+line = drawpad.create_line(250,220,250,260)
+line = drawpad.create_line(220,240,280,240)
 
 root.mainloop()
